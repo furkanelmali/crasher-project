24097ef2de35adf74f0f9bffad2a894d44cb7013
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2023-04-16 17:26:10 +0300

    Loading Bar Update
    Loading Page added, some bugs fixed

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -26,7 +26,7 @@ public class UIManager : MonoBehaviour
     IntersAd intersAd;
     BannerAd banner;
 
-    public LoadingPanel loadingPanel;
+    LoadingPanel loadingPanel;
     public MarketSystem ms;
 
     Sounds sounds;
