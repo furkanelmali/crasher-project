4b5dcddfdc7da24f7404b4581f894ab59ae31cbe
Furkan Elmalı <46634314+furkanelmali@users.noreply.github.com>
2023-05-24 16:25:40 +0300

    laser mechanic v2

--- Assets/Scripts/LaserMechanic.cs
+++ Assets/Scripts/LaserMechanic.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(LineRenderer))]
 public class LaserMechanic : MonoBehaviour
 {
+    public DynamicJoystick joystick;
     public Transform laserOrigin;
+    public GameObject laserRotater;
     public Camera fpsCam;
     public float range = 10f;
     public float duration = 1f;
     public float fireRate = 1f;
+
+    public float rotationspeed = 5f;
     
     float fireTimer;
     LineRenderer laserLine;
@@ -25,7 +29,7 @@ public class LaserMechanic : MonoBehaviour
     void Update()
     {
         processLaser();
-        
+        laserRotaterHandler();
     }
 
     void processLaser()
@@ -34,14 +38,15 @@ public class LaserMechanic : MonoBehaviour
         if(fireTimer >= fireRate && Input.GetButton("Fire1"))
         {
             laserLine.SetPosition(0, laserOrigin.position);
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0f));
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-
+            laserLine.SetPosition(1, hit.point);
         }
         else
         {   
-            return;
+            laserLine.SetPosition(1,rayOrigin + (fpsCam.transform.forward * range));
         }
         StartCoroutine(fireLaser());
         fireTimer = 0f;
@@ -56,6 +61,12 @@ public class LaserMechanic : MonoBehaviour
         laserLine.enabled = true;
         yield return new WaitForSeconds(duration);
         laserLine.enabled = false;
+    }
+
+    void laserRotaterHandler()
+    {
+        if(laserRotater.transform.rotation.z < 45f && laserRotater.transform.rotation.z > -45f)
+        {laserRotater.transform.Rotate(0,0,joystick.Vertical*rotationspeed);}
     }
 }
 
